c1d0a6d26026ebf45acc10a59389f2d43ecc1ecf
benjaminkahl <benjaminkahl@fu-berlin.de>
2019-12-05 13:53:31 +0100

    A 6-1-1

--- main.c
+++ main.c
@@ -21,7 +21,7 @@ int main ( void )
     // Start der RTC  falls diese noch
     // nicht initialisiert war wird
     // die RTC mit der LSE-Taktquelle aktiviert
-    // start_RTC();
+    start_RTC();
 
     // Anmeldung beim WLAN Access Point
     // SSID: MPP_IoT
@@ -37,14 +37,37 @@ int main ( void )
     //CoStartOS ();
 
 	// Beispiel für die Loesung einer Aufgabe
-    //init_leds();
     init_usart_2();
-    init_usart_2_irq();
 
-    //green_LED_OFF;
-
+    // Anlegen der Structs für aktuelle Daten
+    RTC_TimeTypeDef  RTC_Time_Aktuell;      //  Zeit
+    RTC_DateTypeDef  RTC_Date_Aktuell;      //  Datum
 
     while(1) {
+      wait_mSek(1000);
 
+      char data[50] = {0};
+
+      RTC_GetDate(RTC_Format_BIN, &RTC_Date_Aktuell);
+      RTC_GetTime(RTC_Format_BIN, &RTC_Time_Aktuell);
+
+      sprintf(data,
+        "\r\nDATE: %.2d-%.2d-%.2d-%.2d",
+        RTC_Date_Aktuell.RTC_Year,
+        RTC_Date_Aktuell.RTC_Month,
+        RTC_Date_Aktuell.RTC_Date,
+        RTC_Date_Aktuell.RTC_WeekDay
+      );
+      usart_2_print(data);
+
+
+      sprintf(data,
+        "\r\nTIME: %.2d:%.2d:%.2d:%.2d",
+        RTC_Time_Aktuell.RTC_Hours,
+        RTC_Time_Aktuell.RTC_Minutes,
+        RTC_Time_Aktuell.RTC_Seconds,
+        RTC_Time_Aktuell.RTC_H12
+      );
+      usart_2_print(data);
     }
 }
