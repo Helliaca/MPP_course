455a0fa8bf91aa190bcfa7fc592cd7c9566fafe5
benjaminkahl <benjaminkahl@fu-berlin.de>
2020-02-13 15:38:58 +0100

    A12-1-2 Lokalisierung einmalig

--- main.c
+++ main.c
@@ -21,7 +21,7 @@ int main ( void )
     // Start der RTC  falls diese noch
     // nicht initialisiert war wird
     // die RTC mit der LSE-Taktquelle aktiviert
-    start_RTC();
+    //start_RTC();
 
     // Anmeldung beim WLAN Access Point
     // SSID: MPP_IoT
@@ -37,30 +37,21 @@ int main ( void )
     //CoStartOS ();
 
     // Beispiel für die Loesung einer Aufgabe
-    //DW1000_init();
-
-    init_tasten();
-    init_taste2_irq();
 
     init_usart_2();
-    init_BEEPER();
 
     // Initialisierung des DW1000 Transceivers
-    // und anmeldung der Eventhandler
     DW1000_init();
+    // DW1000 in den Deep Sleep versetzen um Strom zu sparen
+    dw1000_idle();
+    dw1000_entersleep();
+    //...
+    // Aufruf der Funktion zur Positionsbestimmung
+    // innerhalb der Funktion erfolgt nach der Messung
+    // die Ausgabe der Entfernungen über die Serielle Schnittstelle
+    position();
 
     while(1) {
-    	wait_mSek(500);
-
-    	dw1000_idle();
-		dw1000_entersleep();
-    	float d = distanz(20);
-
-    	if(d>2.0f) {
-    		beep(200, 500, 0);
-    	} else {
-    		beep(80, 500, 0);
-    	}
     }
 
 }
